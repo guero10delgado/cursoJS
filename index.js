616f26badf7556f6d5a3ac648da059c8de7398d1
guero10delgado <60528507+guero10delgado@users.noreply.github.com>
2022-04-01 15:37:28 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,112 @@
-var numero = 10;
-let numero_2 = 20;
-
-var cadena = 'Delgado';
-var bandera = true;
 
 //Comentario
 
 /* 
-Todo
-esto es 
-un comentario
+   Todo
+   esto es 
+   un comentario
 */
 
-cadena = 'Otro nombre';
-
+//alert("Hola, me llamo " + cadena);
 const Nombre = 'Jorge';
 
-//alert("Hola, me llamo " + cadena);
-console.log("Hola, me llamo " + Nombre);
+
+/*
+   TIPOS DE DATOS
+*/ 
+
+// String (cadenas)
+var nombre = 'Jorge' ;
+
+//Number
+var numero_entero = 10;
+var numero_flotante = 10.5;
+
+//Boolean
+var ban1 = true;
+var ban2 = false;
+
+//undefined
+var saludo;
+
+//Object
+var persona = {
+   nombre: 'Jorge',
+   edad: '28',
+   ciudad: 'Gdl'
+}
+
+
+console.log('String ' + nombre);
+console.log('Numero entero ' + numero_entero);
+console.log('Numero Flotante ' + numero_flotante);
+console.log('True: ' + ban1);
+console.log('False ' + ban2);
+console.log(saludo);
+console.log(persona);
+console.log(persona.nombre);
+
+
+/*
+   SCOPE O ALCANCE
+*/
+
+// Scope local
+function hello(){
+   let hola = 'Hola a todos';
+   console.log(hola);
+   hola = 'hola 2';
+   console.log(hola);
+}
+
+hello();
+//console.log(hola);
+
+
+//Scope Global
+
+var animal = 'Perro';
+
+function ladrar() {
+   animal = 'Gato';
+   console.log('1.- ' + animal);
+}
+
+ladrar();
+console.log('2.- ' + animal);
+
+
+
+ /* 
+   DIFERENCIAS ENTRE let y var
+ */ 
+
+
+
+// Scope sin errores
+let a = 'Esto esta fuera del bloque';
+
+if(true){
+   let a = 'Esto esta dentro del bloque';
+   console.log('Dentro: ' + a); //
+}
+
+console.log('Fuera: ' + a); //
+
+
+
+
+
+// Scope con errores
+let b = 'Esto esta fuera del bloque';
+
+if(true){
+   console.log('Dentro: ' + a); //
+}
+
+let b = 'Esto esta dentro del bloque';
+console.log(b);
+
+
+
 
