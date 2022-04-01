af1a1b825db445273549eb31bc346129426ab989
guero10delgado <60528507+guero10delgado@users.noreply.github.com>
2022-04-01 13:08:10 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+var numero = 10;
+let numero_2 = 20;
+
+var cadena = 'Delgado';
+var bandera = true;
+
+//Comentario
+
+/* 
+Todo
+esto es 
+un comentario
+*/
+
+cadena = 'Otro nombre';
+
+const Nombre = 'Jorge';
+
+//alert("Hola, me llamo " + cadena);
+console.log("Hola, me llamo " + Nombre);
+
